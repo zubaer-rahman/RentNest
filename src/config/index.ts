b86af25452cc3ab30b24836357183a6dad181062
zubaer-rahman <zubaer.rahman.cse@gmail.com>
2026-07-12 10:47:53 +0600

    integrate Prisma database connection and standardize port configuration in server initialization

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,7 +5,7 @@ dotenv.config({ path: path.join((process.cwd(), '.env')) });
 
 export default {
   env: process.env.NODE_ENV,
-  port: process.env.PORT,
+  port: Number(process.env.PORT) || 5000,
   jwt_access_secret: process.env.JWT_ACCESS_SECRET,
   jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
   bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,20 @@
-import "dotenv/config";
 import app from "./app";
+import "dotenv/config";
+import config from "./config";
+import prisma from "./lib/prisma";
 
-const PORT = Number(process.env.PORT) || 5000;
-
+const PORT = config.port;
 async function main() {
   try {
+    await prisma.$connect();
+    console.log("Database connected successfully!");
+
     app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+      console.log(`Server is running on port ${PORT}`);
     });
-  } catch (error: any) {
-    console.log(`Error starting the server ${error}`);
+  } catch (error) {
+    console.log(`Error starting  the server ${error}`);
+    await prisma.$disconnect();
     process.exit(1);
   }
 }
